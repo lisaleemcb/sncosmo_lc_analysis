54d18d8adb3b43609fc7df4a43968b78563b0acc
Lisa McBride <lisaleemcb@gmail.com>
2016-05-16 12:27:01 -0700

    actually lca_packet class is here

---
+++ lca_packet.py
@@ -0,0 +1,29 @@
+import sncosmo
+import triangle
+from astropy.table import Table
+import math
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import numpy as np
+import collections
+import lightcurve_analysis as lca
+
+class LCA_packet(object):
+    "class to generate packet of all the fits I want for extinction analysis"
+    def __init__(self, sn_id, model, obstable):
+
+        # test paramter
+        self.word = word
+
+        self.lc_noext_fit_noext
+        self.lc_noext_fit_ext
+        self.lc_ext_fit_noext
+        self.lc_ext_fit_ext
+
+    # test function
+    def test(self):
+        print "hello," + self.word
+
+    def make_packet(self)
+
+    lca.LCA()
